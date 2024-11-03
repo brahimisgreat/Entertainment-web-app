c98660ceb7835d2df96ec85ee3a6d4818146b0a3
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-11-02 23:44:39 -0400

    fd

--- vite-project/src/Components/Pages/Home/Trending.tsx
+++ vite-project/src/Components/Pages/Home/Trending.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import data from '../../../data.json'
+import movieIcon from '../../../assets/assets/icon-category-movie.svg'
 
 export const Trending = () => {
+    console.log(data)
   return (
-    <div>Trending</div>
+    <div className='text-white'>
+        <h3>Trending</h3>
+
+        <div className='h-36 w-60'>
+            <div>
+
+            </div>
+            <div>
+                <div >
+
+                </div>
+                <h3>{'beyond earth'}</h3>
+            </div>
+
+        </div>
+
+
+    </div>
   )
 }
